733452017634c90b988c76746336e29675347c2e
GuilhermeBSilva2002 <gbarbosasilva14@gmail.com>
2024-07-29 14:51:16 -0400

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Cliente</title>
-    <link rel="stylesheet" href="personalizado.css">
+    <link rel="stylesheet" href="/personalizado.css">
 </head>
 
 
