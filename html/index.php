fe1aaf0838c82930492f762f6e8d26a2fce22c83
GuilhermeBSilva2002 <gbarbosasilva14@gmail.com>
2024-08-07 13:22:27 -0400

    BANCO DE DADOS
    banco

--- html/index.php
+++ html/index.php
@@ -1,15 +1,4 @@
 <?php
 
-$dsn = 'pgsql:host=localhost;port=5432;dbname=senac';
-$usuario = 'gbsilva2002';
-$senha = 'guilherme10';
+require __DIR__ . '/../vendor/autoload.php';
 
-
-try {
-    $pdo = new PDO($dsn, $usuario, $senha);
-    // Configurar o PDO para lançar exceções em caso de erro
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexão bem-sucedida";
-} catch (PDOException $e) {
-    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
-}
