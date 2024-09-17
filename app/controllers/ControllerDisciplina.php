90de36e9e880513450cf33c4c7342d0b36c95d92
GuilhermeBSilva2002 <gbarbosasilva14@gmail.com>
2024-09-17 16:29:44 -0400

    commit

---
+++ app/controllers/ControllerDisciplina.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\controllers;
+
+use app\database\builder\SelectQuery;
+
+class ControllerDisciplina extends Base
+{
+    public function lista($request, $response)
+    {
+        try {
+            $disciplinas = (array) SelectQuery::select()
+                ->from('disciplina')
+                ->fetchAll();
+            $TemplateData = [
+                'titulo' => 'Lista de Disciplinas',
+                'disciplinas' => $disciplinas
+            ];
+            return $this->getTwig()
+                ->render($response, $this->setView('listadisciplina'), $TemplateData)
+                ->withHeader('Content-Type', 'text/html')
+                ->withStatus(200);
+        } catch (\Exception $e) {
+            throw new \Exception("Restrição: " . $e->getMessage(), 1);
+        }
+    }
+    public function cadastro($request, $response)
+    {
+        $TemplateData = [
+            'acao' => 'c',
+            'titulo' => 'Lista de Disciplinas'
+        ];
+        return $this->getTwig()
+            ->render($response, $this->setView('disciplina'), $TemplateData)
+            ->withHeader('Content-Type', 'text/html')
+            ->withStatus(200);
+    }
+    public function alterar($request, $response, $args)
+    {
+        var_dump($args);
+        die;
+        $TemplateData = [
+            'acao' => 'e',
+            'titulo' => 'Lista de Disciplinas'
+        ];
+    }
+}
---
+++ app/controllers/ControllerHome.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+
+class ControllerHome extends Base
+{
+    public function home($request, $response)
+    {
+        $TempleteData = [
+            'titulo' => 'Você esta no inicio, jovem gafanhoto'
+        ];
+        return $this->getTwig()
+            ->render($response, $this->setView('pagina-inicial'), $TempleteData)
+            ->withHeader('Content-Type', 'text/html')
+            ->withStatus(200);
+    }
+}
