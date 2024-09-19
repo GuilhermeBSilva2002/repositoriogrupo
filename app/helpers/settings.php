fca79c14a31df601b77483247dc63156354cf01f
GuilhermeBSilva2002 <gbarbosasilva14@gmail.com>
2024-09-19 15:53:58 -0400

    Commit

--- app/helpers/settings.php
+++ app/helpers/settings.php
@@ -1,7 +1,18 @@
 <?php
+#Cria e inicializa uma sessão no servidor.
+session_start();
+#Cria uma variável de sessão com nome e idade
+$_SESSION['nome'] = 'Guilherme';
+$_SESSION['idade'] = 22;
+
+echo $_SESSION['nome'];
+echo $_SESSION['idade'];
 #Esta constante tem o seguiunte valor: string(8) "/var/www"
 define('ROOT', dirname(__FILE__, 3));
 #Esta constante tem o seguiunte valor: string(18) "/var/www/app/views"
 define('DIR_VIEW', ROOT . '/app/views');
 
 define('EXT_VIEWS', '.html');
+
+
+
